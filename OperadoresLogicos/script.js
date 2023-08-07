b37d94c89b53c75014be45e87689cef4aec33a5e
Programazion <programazion1@gmail.com>
2023-08-06 21:14:02 -0500

    Operadores Lógicos

---
+++ OperadoresLogicos/script.js
@@ -0,0 +1,21 @@
+console.log('Loading JavaScript');
+//Validaciones del formaulario
+prompt('Bienvenid@ ingresa tu nombre');
+
+function validacion(){
+    console.log('Validando');
+    let name= document.getElementById('nombre').value;
+    let email= document.getElementById('email').value;
+    let tel= document.getElementById('telefono').value;
+    datos = [
+            'Nombre =>',name,
+             'email=>', email,
+             'telefono=>', tel];
+    
+    if(datos[0]=="" || datos[1]=="" || datos[2]==""){
+        alert('faltan datos');      
+    }else{
+        alert('sus datos fueron enviados'); 
+        console.log('datos enviados', datos);   
+    }
+}
